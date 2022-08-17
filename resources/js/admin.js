09cdd584a9fde486bf7ca6a7ae5f24a648fd5629
BinotaLIU <binota@binota.org>
2022-08-17 11:10:10 +0800

    chore: use `laravel-vite-plugin/inertia-helpers` to resolve pages

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,3 +1,4 @@
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/inertia-vue3'
 
@@ -6,16 +7,15 @@ import { InertiaProgress } from '@inertiajs/progress'
 import components from '../components'
 import AdminLayout from '../layouts/Admin.vue'
 
+// noinspection JSIgnoredPromiseFromCall
 createInertiaApp({
-  resolve: (name) => {
-    const [dir, file] = name.split('/')
-    return import(`../pages/${dir}/${file}.vue`)
+  resolve: (name) =>
+    resolvePageComponent(`../pages/${name}.vue`, import.meta.glob('../pages/**/*.vue'))
       .then((component) => {
-        component.default.layout = component.default.layout || AdminLayout
+        component.default.layout ||= AdminLayout
 
         return component
-      })
-  },
+      }),
   setup({ el, App, props, plugin }) {
     const app = createApp({ render: () => h(App, props) })
     app.use(plugin)
