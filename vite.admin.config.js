1fe79ab8f0bdf230790e0ec6dffb23eee2d37934
BinotaLIU <binota@binota.org>
2022-09-18 14:42:17 +0800

    feat: chunks

--- vite.admin.config.js
+++ vite.admin.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import laravel from 'laravel-vite-plugin'
 import vue from '@vitejs/plugin-vue'
-import vueMacros from 'unplugin-vue-define-options/vite'
 
 import 'dotenv/config'
+import laravel from 'laravel-vite-plugin'
+import vueMacros from 'unplugin-vue-define-options/vite'
+import { defineConfig } from 'vite'
 
 const fs = require('node:fs')
 const path = require('node:path')
@@ -36,7 +36,39 @@ export default defineConfig({
       '@': path.resolve(__dirname, './resources'),
     }
   },
+
   ...(server === null ? {} : { server }),
+
+  build: {
+    rollupOptions: {
+      manualChunks(id) {
+        const chunksMap = {
+          'node_modules/@heroicons': 'vendor-icons',
+          'node_modules': 'vendors',
+          'resources/components': 'components',
+          'resources/js': 'utils',
+          'resources/layouts/Auth': 'auth',
+          'resources/pages/Articles': 'campaign',
+          'resources/pages/Auth': 'auth',
+          'resources/pages/Banners': 'campaign',
+          'resources/pages/Categories': 'catalog',
+          'resources/pages/Products': 'catalog',
+          'resources/pages/Users': 'users',
+          'resources/pages': 'misc',
+          'resources/': 'misc',
+        }
+
+        return (
+          Object
+            .entries(chunksMap)
+            .find(([pattern]) => id.includes(pattern)) || []
+        )
+          .slice(1)
+          .find(() => true)
+      }
+    }
+  },
+
   plugins: [
     laravel({
       input: [
