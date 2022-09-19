36efdfea32bc3b84af2bf75b62d4094a7c759faf
BinotaLIU <binota@binota.org>
2022-09-19 15:02:56 +0800

    chore: adjust life style

--- resources/views/life.blade.php
+++ resources/views/life.blade.php
@@ -4,16 +4,16 @@
             @foreach($blocks as $block)
                 <div class="flex items-center gap-x-4">
                     @if($block['text_position'] === 'right')
-                        <div class="w-1/8">
+                        <div class="w-1/6 shrink">
                             <img src="{{ $block['image'] }}" alt="{{ $block['image_description']['zh_Hant_TW'] }}" />
                         </div>
                     @endif
-                    <div class="w-7/8 flex flex-col gap-2">
+                    <div class="w-5/6 flex-grow flex flex-col gap-2">
                         <h2 class="text-2xl font-bold text-pearl-800">{{ $block['title']['zh_Hant_TW'] }}</h2>
                         <p class="text-sm text-wood-700">{{ $block['text']['zh_Hant_TW'] }}</p>
                     </div>
                     @if($block['text_position'] === 'left')
-                        <div class="w-1/8">
+                        <div class="w-1/6 shrink">
                             <img src="{{ $block['image'] }}" class="w-full" alt="{{ $block['image_description']['zh_Hant_TW'] }}" />
                         </div>
                     @endif
